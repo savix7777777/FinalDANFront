efe75e43ae0b4a4d8569e07c5481786f80e4ef97
Sergey Savchenko <savchenkoserhii123@gmail.com>
2020-10-30 00:56:35 +0200

    Updates

---
+++ precache-manifest.b687ff158c96883a9d8e107185f9527f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc756466b659a5f4bdc87353655fc79",
+    "url": "/FinalDANFront/index.html"
+  },
+  {
+    "revision": "02376a1028a48671f220",
+    "url": "/FinalDANFront/static/css/main.d74a4117.chunk.css"
+  },
+  {
+    "revision": "b40b60151a4f27cbf4f4",
+    "url": "/FinalDANFront/static/js/2.d572dd62.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/FinalDANFront/static/js/2.d572dd62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02376a1028a48671f220",
+    "url": "/FinalDANFront/static/js/main.5d829757.chunk.js"
+  },
+  {
+    "revision": "7201171e1c9df0226307",
+    "url": "/FinalDANFront/static/js/runtime-main.b349f4ed.js"
+  },
+  {
+    "revision": "33f225b8f5f7d6b34a0926f58f96c1e9",
+    "url": "/FinalDANFront/static/media/2212 .33f225b8.ttf"
+  }
+]);
